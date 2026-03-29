4592de0b45b0d9cc323e15b6d0261220e4e2ddf4
dan <dan@foohq.io>
2026-03-29 20:14:33 +0200

    devbox: Support template override via a front matter

--- devbox.sh
+++ devbox.sh
@@ -5,10 +5,16 @@ get_mds() {
     find "$CONTENT_DIR" -type f -name "*.md"
 }
 
+get_template() {
+   awk '/^template:/ { print $2; found=1; exit }
+           END { if (!found) print "default.html" }' "$1"
+}
+
 while read input
 do
     output="${input#*/}"
     output="$OUTPUT_DIR/${output%.md}.html"
-    echo "Building $input => $output"
-    mkdir -p "$(dirname "$output")" && pandoc -d default.yaml -o "$output" "$input"
+    template="$(get_template "$input")"
+    echo "Building $input => $output (template: $template)"
+    mkdir -p "$(dirname "$output")" && pandoc -d default.yaml --template "$template" -o "$output" "$input"
 done < <(get_mds)
